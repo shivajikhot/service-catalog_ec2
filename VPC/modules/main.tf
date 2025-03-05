1a3721c7a7c90286f5f171fac607802422a19205
shivajikhot <shivajkv@gmail.com>
2025-03-05 16:27:14 +0530

    Create main.tf

---
+++ VPC/modules/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Call the VPC module
+module "vpc" {
+  source          = "../../modules/vpc"
+  vpc_name        = var.vpc_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  azs             = var.azs
+}
