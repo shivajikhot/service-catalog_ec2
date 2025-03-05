33691d370504a9284865e2cae0b93ce0d828628d
shivajikhot <shivajkv@gmail.com>
2025-03-05 16:33:35 +0530

    Update backend.tf

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend-statefil"  
     key            = "SC_EC2/development/terraform.tfstate"
-    region         = "us-east-1"            
+    region         = "us-west-1"            
     encrypt        = true
   }
 }
