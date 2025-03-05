9e380e7b02e3b2366d69d409787ff7149db4f6a5
shivajikhot <shivajkv@gmail.com>
2025-03-05 16:17:30 +0530

    Create variables.tf

---
+++ VPC/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
