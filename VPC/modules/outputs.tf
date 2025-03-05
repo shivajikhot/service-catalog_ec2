4d2f1a25b75723df301ee49e19c0ebf6e4c00dc3
shivajikhot <shivajkv@gmail.com>
2025-03-05 16:30:28 +0530

    Create outputs.tf

---
+++ VPC/modules/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
