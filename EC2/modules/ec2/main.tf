547e79066325a47f7871496c435791e1279ebdba
shivajikhot <shivajkv@gmail.com>
2025-03-06 10:04:34 +0530

    Update main.tf

--- EC2/modules/ec2/main.tf
+++ EC2/modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id     = element(var.subnet_ids, count.index)
   tags = {
     Name = var.instance_name
   }
